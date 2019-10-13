d3aa1718b1f871333bc33dec9981a6224a0310c3
Nguyen Tien Hai <hai.phihungl2002@gmail.com>
2019-10-13 16:51:57 +0700

    cm API

--- app/src/main/java/com/vpbank/webapisimple/JsonWebAPI.java
+++ app/src/main/java/com/vpbank/webapisimple/JsonWebAPI.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JsonWebAPI extends AppCompatActivity {
     String json = "{ \"temp\": 289.92, \"pressure\": 1009, \"humidity\": 92, \"temp_min\": 288.71, \"temp_max\": 290.93 }";
+    String jsonArray = "[ { \"id\": 300, \"main\": \"Drizzle\", \"description\": \"light intensity drizzle\", \"icon\": \"09d\" }, { \"id\": 200, \"main\": \"Manh\", \"description\": \"light intensity drizzle\", \"icon\": \"09d\" } ]";
 
     TextView tvJson;
     String result;
@@ -31,5 +33,28 @@ public class JsonWebAPI extends AppCompatActivity {
             e.printStackTrace();
         }
 
+
+        getJsonArray();
+
+    }
+
+    private void getJsonArray() {
+        try {
+            JSONArray jArray = new JSONArray(jsonArray);
+
+            for (int i = 0; i < jArray.length(); i++) {
+                JSONObject jsonObject = jArray.getJSONObject(i);
+                String id = jsonObject.getString("id");
+                String main = jsonObject.getString("main");
+                String des = jsonObject.getString("description");
+                String icon = jsonObject.getString("icon");
+
+                result += "\nid: " + id + " main: " + main + " des: " +
+                        des + " icon: " + icon;
+            }
+            tvJson.setText(result);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
